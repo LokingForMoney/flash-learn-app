2b87bbb559aac49cfc0e529eeeab763a269a8f84
Anna <devilflow3@gmail.com>
2024-11-23 18:18:18 +1000

    gandon

--- api/telegram/index.js
+++ api/telegram/index.js
@@ -1,17 +1,28 @@
-const TelegramBot = require('node-telegram-bot-api');
+import TelegramBot from 'node-telegram-bot-api';
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
 
-exports.handler = async function(event, context) {
-  const body = JSON.parse(event.body);
-  
-  if (body.message) {
-    const chatId = body.message.chat.id;
-    await bot.sendMessage(chatId, 'Тест бота');
+export const handler = async function(event, context) {
+  try {
+    console.log('=== НОВЫЙ ЗАПРОС ===');
+    const body = JSON.parse(event.body);
+    console.log('ДАННЫЕ:', body);
+
+    if (body.message) {
+      const chatId = body.message.chat.id;
+      console.log('CHAT ID:', chatId);
+      await bot.sendMessage(chatId, 'Тест бота');
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true })
+    };
+  } catch (error) {
+    console.log('ОШИБКА:', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ ok: false })
+    };
   }
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ok: true })
-  };
 };
