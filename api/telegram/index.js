b0f871b28c8a8912fbbda35b697c71c44fcac51f
Anna <devilflow3@gmail.com>
2024-11-23 18:46:28 +1000

    )))

--- api/telegram/index.js
+++ api/telegram/index.js
@@ -1,7 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
-const webAppUrl = 'https://flashclearn.netlify.app/2';
+const webAppUrl = 'https://flashclearn.netlify.app/';
 
 export const handler = async function(event, context) {
   try {
